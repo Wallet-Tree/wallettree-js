b0e07218d12c3ea3383745239c6f0b4d47b574f1
Joshua Blew <joshua@normalfinance.io>
2023-03-03 16:40:23 -0700

    begin adding web3 to WalletTreeSDK

--- packages/sdk/src/lib/sdk.ts
+++ packages/sdk/src/lib/sdk.ts
@@ -1,7 +1,44 @@
 import { walletSdk } from './services/sdk.wallet'
 import { WalletTreeUrlArg } from '@wallettree/api-client'
+import Web3 from 'web3'
+import { sha3 } from 'web3-utils'
 
-export const WalletTreeSDK = (args: { apiKey: string; url?: WalletTreeUrlArg }) => {
+export const WalletTreeSDK = (args: {
+  apiKey: string
+  provider: any
+  fromPrivateKey?: string
+  url?: WalletTreeUrlArg
+}) => {
+  const web3 = new Web3(args.provider)
+
+  if (args.fromPrivateKey) {
+    web3.eth.accounts.wallet.add(args.fromPrivateKey)
+    web3.eth.defaultAccount = web3.eth.accounts.wallet[0].address
+  }
+
+  // const contract = new web3.eth.Contract(ResolverContract.abi, walletSecrets.resolverContractAddress)
+
+  const createPrimaryResolver = (identifier: string, wallets: any, allowServer: boolean) => {
+    const hash = sha3(identifier)
+    // const tx = contract.methods.createResolver(hash, 'cid', 'signature', allowServer)
+
+    // tx.send({
+    //   from: web3.eth.defaultAccount,
+    //   gas: tx.estimateGas() * 2,
+    // })
+    //   .on('transactionHash', (txHash) => {})
+    //   .on('error', (error, receipt) => {})
+  }
+
+  // updatePrimaryResolverCid() {},
+
+  // enablePrimaryResolverAllowServer() {},
+
+  // deletePrimaryResolver() {},
+
+  // createSecondaryResolver() {},
+
+  // deleteSecondaryResolver() {},
   return {
     wallet: walletSdk,
   }
