2013b07cf02c317dc5d1e0788dc16ba1e91eb3d0
Joshua Blew <joshua@normalfinance.io>
2023-03-03 16:40:02 -0700

    removed unneeded files

--- packages/shared/core/src/lib/address.common.ts
+++
@@ -1,3 +0,0 @@
-export const ADDRESS_PREFIX = {
-  EVM: '0x',
-}
--- packages/shared/core/src/lib/http.common.ts
+++ packages/shared/core/src/lib/http.common.ts
@@ -1,33 +1,5 @@
 import axios from 'axios'
-import { Blockchain, EvmBasedBlockchain } from './models/Blockchain'
-import { fetchAdapter, WALLETTREE_API_CONSTANTS } from '@wallettree/api-client'
-import { blockchainHelper } from './blockchain.common'
-
-const EndpointsMapping: Record<Blockchain, string> = {
-  HARMONY: 'one',
-  BTC: 'bitcoin',
-  DOGE: 'dogecoin',
-  EGLD: 'egld',
-  FLOW: 'flow',
-  LTC: 'litecoin',
-  NEO: 'neo',
-  POLYGON: 'polygon',
-  SOL: 'solana',
-  TRON: 'tron',
-  VET: 'vet',
-  XRP: 'xrp',
-  ETH: 'ethereum',
-  CELO: 'celo',
-  BSC: 'bsc',
-  ALGO: 'algorand',
-  ADA: 'ada',
-  BCH: 'bcash',
-  XDC: 'xdc',
-  XLM: 'xlm',
-  KCS: 'kcs',
-  KLAY: 'klaytn',
-  BNB: 'bnb',
-}
+import { fetchAdapter } from '@wallettree/api-client'
 
 const isWebWorker =
   typeof self === 'object' && self.constructor && self.constructor.name === 'DedicatedWorkerGlobalScope'
@@ -37,11 +9,4 @@ export const httpHelper = {
   post: axios.post,
   axios: axios.create({ adapter: isWebWorker ? fetchAdapter : undefined }),
   CancelToken: axios.CancelToken,
-  web3Endpoint: (blockchain: EvmBasedBlockchain, url: string, apiKey: string) => {
-    return `${url}/${WALLETTREE_API_CONSTANTS.API_VERSION}/${EndpointsMapping[blockchain]}/web3/${apiKey}`
-  },
-  rpcEndpoint: (blockchain: Blockchain, url: string, apiKey: string) => {
-    const chain = blockchainHelper.getDefaultCurrencyByBlockchain(blockchain)
-    return `${url}/${WALLETTREE_API_CONSTANTS.API_VERSION}/blockchain/node/${chain}/${apiKey}`
-  },
 }
