82f4abe93ef00ebd6d6a2c462f7ee504679fb9fa
Joshua Blew <joshua@normalfinance.io>
2023-04-06 18:39:51 -0500

    add initial SDK tests

--- packages/sdk/tests/main.test.ts
+++ packages/sdk/tests/main.test.ts
@@ -1,73 +1,116 @@
+// Modules
 import { WalletTreeSDK } from '../../../dist/packages/sdk/src'
 
+// Types
+import { Wallet } from 'dist/packages/sdk/src/lib/models/Wallet'
+import { WalletChain } from 'dist/packages/sdk/src/lib/models/WalletChain'
+import { WalletProvider } from 'dist/packages/sdk/src/lib/models/WalletProvider'
+
+// Config
+const apiKey = '123'
+const ceramicUrl = 'https://...'
+
 describe('WalletTreeSDK', () => {
+    let walletTree: WalletTreeSDK
+
+    beforeAll(async () => {
+        walletTree = new WalletTreeSDK({ apiKey: apiKey, ceramicUrl: ceramicUrl })
+    })
+
     describe('SDK', () => {
         it('should create a new SDK instance', async () => {
-            expect(true).toEqual(1)
+            expect(walletTree.ceramic).toBeTruthy()
+            expect(walletTree.composeClient).toBeTruthy()
         })
 
         it('should authenticate the SDK instance', async () => {
-            expect(true).toEqual(1)
+            await walletTree.authenticate()
+            expect(walletTree.ceramic.did).toBeTruthy()
         })
 
         it('should search for a profile using an email', async () => {
-            expect(true).toEqual(1)
+            const email = 'joshua@wallettre.me'
+            const { profile } = await walletTree.search(email)
+            // TODO: this will fail as userId is an internal value to WalletTree and NOT the same as email
+            expect(profile.userId).toEqual(email)
         })
 
         it('should search for a profile using a social username', async () => {
-            expect(true).toEqual(1)
+            const twitterUsername = 'wallettreeme-twitter'
+            const { profile } = await walletTree.search(twitterUsername)
+            // TODO: this will fail as userId is an internal value to WalletTree and NOT the same as a social username
+            expect(profile.userId).toEqual(twitterUsername)
         })
     })
 
     describe('Profile', () => {
-        let walletTree: WalletTreeSDK
-
-        beforeAll(async () => {
-            walletTree = new WalletTreeSDK({ apiKey: '', ceramicUrl: '' })
-        })
-
         it('should create a new profile', async () => {
-            expect(true).toEqual(1)
+            const userId = 'abc123'
+            const profile = await walletTree.profile.create(userId)
+            expect(profile.userId).toEqual(userId)
         })
 
         it('should update profile privacy', async () => {
-            expect(true).toEqual(1)
+            const privacy = await walletTree.profile.get()
+            const profile = await walletTree.profile.setPrivacy(!privacy)
+            expect(profile.privacy).toEqual(!privacy)
         })
     })
 
     describe('Wallet', () => {
-        let walletTree: WalletTreeSDK
+        let profile: any
+        let newWallet
 
         beforeAll(async () => {
-            walletTree = new WalletTreeSDK({ apiKey: '', ceramicUrl: '' })
+            profile = await walletTree.profile.get()
         })
 
         it('should create a new wallet', async () => {
-            expect(true).toEqual(1)
+            const wallet: Wallet = {
+                address: '0x0000000000000000000000000000000000000000',
+                name: 'Test name',
+                description: 'Test description',
+                provider: WalletProvider.ETHEREUM,
+                chain: WalletChain.METAMASK,
+                primary: false,
+                private: false,
+                favorite: false,
+                // TODO: add createdAt, updatedAt?
+            }
+            newWallet = await walletTree.wallets.create(profile.profileId, wallet)
+            expect(newWallet.address).toEqual(wallet.address)
         })
 
         it('should update a wallet name', async () => {
-            expect(true).toEqual(1)
+            const name = 'For Vitalik'
+            const wallet = await walletTree.wallets.updateName(newWallet.id, name)
+            expect(wallet.name).toEqual(name)
         })
 
         it('should update a wallet description', async () => {
-            expect(true).toEqual(1)
+            const description = 'Because I love him'
+            const wallet = await walletTree.wallets.updateDescription(newWallet.id, description)
+            expect(wallet.description).toEqual(description)
         })
 
         it('should update a wallet primary status', async () => {
-            expect(true).toEqual(1)
+            const wallet = await walletTree.wallets.updatePrimary(newWallet.id, true)
+            expect(wallet.primary).toEqual(true)
         })
 
         it('should update a wallet privacy', async () => {
-            expect(true).toEqual(1)
+            const wallet = await walletTree.wallets.updatePrivacy(newWallet.id, true)
+            expect(wallet.privacy).toEqual(true)
         })
 
         it('should update a wallet favorite status', async () => {
-            expect(true).toEqual(1)
+            const wallet = await walletTree.wallets.updateFavorite(newWallet.id, true)
+            expect(wallet.favorite).toEqual(true)
         })
 
         it('should delete a wallet', async () => {
-            expect(true).toEqual(1)
+            const wallet = await walletTree.wallets.delete(newWallet.id)
+            expect(wallet.deletedAt).toBeTruthy()
         })
     })
 })
