edd800c42cf79259fe283c6e5d9672029bb40782
Joshua Blew <joshua@normalfinance.io>
2023-04-06 18:39:59 -0500

    add initial api-client tests

---
+++ packages/api-client/tests/main.test.ts
@@ -0,0 +1,47 @@
+// Modules
+import { WalletTreeApi } from '../../../dist/packages/api-client/src'
+
+// Config
+const apiKey = '123'
+const url = 'https://dev-api.wallettree.me/v1'
+
+describe('WalletTreeSDK', () => {
+    let walletTreeApi: any
+
+    beforeAll(async () => {
+        walletTreeApi = WalletTreeApi(apiKey, url)
+    })
+
+    describe('SearchService', () => {
+        it('should search for a profile using an email', async () => {
+            const email = 'joshua@wallettre.me'
+            const response = await walletTreeApi.search.search(email)
+            // TODO: this will fail as userId is an internal value to WalletTree and NOT the same as email
+            expect(response.data.profile.userId).toEqual(email)
+        })
+
+        it('should search for a profile using a social username', async () => {
+            const twitterUsername = 'wallettreeme-twitter'
+            const response = await walletTreeApi.search.search(twitterUsername)
+            // TODO: this will fail as userId is an internal value to WalletTree and NOT the same as a social username
+            expect(response.data.profile.userId).toEqual(twitterUsername)
+        })
+    })
+
+    describe('PaymentsService', () => {
+        it("should get a user's payments", async () => {
+            const response = await walletTreeApi.payments.payments()
+            expect(response).toEqual('Coming soon :)')
+        })
+
+        it('should create a request payment', async () => {
+            const response = await walletTreeApi.payments.request()
+            expect(response).toEqual('Coming soon :)')
+        })
+
+        it('should create a send payment', async () => {
+            const response = await walletTreeApi.payments.send()
+            expect(response).toEqual('Coming soon :)')
+        })
+    })
+})
